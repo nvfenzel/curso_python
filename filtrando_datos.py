521e0bd2bcf9c4be40901dce5985a24d9d275cde
nicovfenzel <nicolasvaldiviafenzel@gmail.com>
2022-10-14 04:27:44 -0300

    filtrado

--- filtrando_datos.py
+++ filtrando_datos.py
@@ -73,7 +73,21 @@ DATA = [
 
 
 def run():
-    pass
+    all_python_devs = [worker["name"]
+                       for worker in DATA if worker["language"] == "python"]
+
+    all_platzi_workers = [worker["name"]
+                          for worker in DATA if worker["organization"] == "Platzi"]
+
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+
+    adults = list(map(lambda worker: worker["name"], adults))
+
+    old_people = list(map(lambda worker: worker | {
+                      "old": worker["age"] > 70}, DATA))
+
+    for worker in old_people:
+        print(worker)
 
 
 if __name__ == '__main__':
